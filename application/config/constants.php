044c4c5442ee9aa261b661f86510e029d261b4af
Fred Yang <fredyang001@gmail.com>
2017-03-02 15:40:14 -0800

    closes #6,7 pagination and user roles

--- application/config/constants.php
+++ application/config/constants.php
@@ -3,6 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
+| User Roles
+|--------------------------------------------------------------------------
+|
+| Define two user roles: "guest" and "owner".
+|
+*/
+defined('ROLE_GUEST') OR define('ROLE_GUEST', 'Guest');
+defined('ROLE_OWNER') OR define('ROLE_OWNER', 'Owner');
+/*
+|--------------------------------------------------------------------------
 | Display Debug backtrace
 |--------------------------------------------------------------------------
 |
--- changelog.md
+++ changelog.md
@@ -5,3 +5,8 @@ All notable changes to this project will be documented in this file.
 ### Added
 -Added and completed job 4, maintenance list
     -Yiaoping Shu
+
+Closes #6 (Fred)
+	-Pagination
+Closes #7 (Fred)
+	-User Roles
